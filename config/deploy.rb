a4d92db51105698233cf961aedc00cbcf075077f
siann55 <takodo_exword22@yahoo.co.jp>
2019-05-15 14:46:05 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,9 +8,10 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
+# set :rbenv_path, '$HOME/.rbenv'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/iwatashougo.pem']
+                  keys: ['/Users/abeyoshiaki/.ssh/iwatashougo.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
