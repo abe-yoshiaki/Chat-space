bbdc46aec684f9f84b3b43f5a6b666e7f18cbd9c
siann55 <takodo_exword22@yahoo.co.jp>
2019-05-17 11:15:00 +0900

    imageupload

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'iwatashougo'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/iwatashougo'
+  config.fog_directory  = 'yabe-apptest'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/yabe-apptest'
 end
